160d9dfc77e177539bf3994e180db1f69babd52e
benup <benup211@gmail.com>
2024-08-31 23:29:29 +0545

    Update page title and add website logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/sidebar";
-import { OverviewPage } from "./pages";
+import { OverviewPage, SignInPage,SignUpPage } from "./pages";
 function App() {
+    const isAuthenticated = false;
     return (
         <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
-            <div className="fixed inset-0 z-0">
-                <div className="absolute inset-0 bg-gradient-to-br from-gray-900 opacity-80" />
-                <div className="absolute inset-0 backdrop-blur-sm" />
-            </div>
-            <Sidebar />
-            <Routes>
-                <Route path="/" element={<OverviewPage/>} />
-                <Route path="/products" element={<div>hello</div>} />
-            </Routes>
+            {isAuthenticated ? (
+                <>
+                    <div className="fixed inset-0 z-0">
+                        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 opacity-80" />
+                        <div className="absolute inset-0 backdrop-blur-sm" />
+                    </div>
+                    <Sidebar />
+                    <Routes>
+                        <Route path="/" element={<OverviewPage />} />
+                        <Route path="/smtp-server" element={<div>hello</div>} />
+                    </Routes>
+                </>
+            ) : (
+                <>
+                    <Routes>
+                        <Route path="/login" element={<SignInPage />} />
+                        <Route path="/signup" element={<SignUpPage />} />
+                    </Routes>
+                </>
+            )}
         </div>
     );
 }
